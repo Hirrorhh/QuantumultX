f6aebbca9e82a5b959b94f706cfd8bcfac13020e
06335-韩翰翰 <hanhh@mwclg.com>
2023-03-02 15:53:52 +0800

    AD_Block

--- qx/config/GetBd.js
+++ qx/config/GetBd.js
@@ -10,28 +10,28 @@ hostname=czapp.bestpay.com.cn
 const $ = new Env('中国电信获取body');
 const effective = $request.body.indexOf("BILLCYCLE")
 let loginerror=$.read('Bodyswitch')
-
 !(async () => {await GetBody();})().catch((e) => {$.log(e)}).finally(() => {$.done({});});
-
 async function GetBody() {
-    if (effective==0 &&loginerror==1) {
-      if ($request.body) {
-        $.write($request.body, "Tele_BD")
-        $.write(0,'Bodyswitch')
-        title='中国电信'
-        body='Body写入成功'
-        body1=$request.body
-        $.notice(title,body,body1)
-      }
+  if (effective==0 &&loginerror==1) {
+    if ($request.body) {
+      $.write($request.body, "Tele_BD")
+      $.write(0,'Bodyswitch')
+      title='中国电信'
+      body='Body写入成功'
+      body1=$request.body
+      $.notice(title,body,body1)
     }
+  }
+  else{
+    if(loginerror==0){console.log('当前Body有效，无需获取')}
     else{
-        if(loginerror==0){console.log('当前Body有效，无需获取')}
-        
-        }   
-
+      title='当前操作'
+      body='请点击已用流量'
+      body1=''
+      $.notice(title,body,body1)
+    }
+  }
 }
-
-
 function Env(name) {
   LN = typeof $loon != `undefined`
   SG_STH_SDR = typeof $httpClient != `undefined` && !LN
@@ -41,7 +41,7 @@ function Env(name) {
     if (QX) return $prefs.valueForKey(key)
   }
   write = (key, val) => {
-    if (LN || SG_STH_SDR) return $persistentStore.write(String(key), val); 
+    if (LN || SG_STH_SDR) return $persistentStore.write(String(key), val);
     if (QX) return $prefs.setValueForKey(String(key), val)
   }
   notice = (title, subtitle, message, url) => {
